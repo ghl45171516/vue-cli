13b4b29fd4c3b60b47447028f22466f7f7d9d76e
Mr.Guo <ghl45171516@163.com>
2020-09-18 14:01:20 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports ={
+  devServer:{
+    port:8000,
+    open:true,
+    host:"localhost"
+  },
+  // outputDir:'dist2',
+  // assetsDir:'assets',
+  // indexPath:'out/index.html',
+  // productionSourceMap:false
+}
